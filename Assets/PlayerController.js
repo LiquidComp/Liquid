11daa9fe75fad70bb03eb97329527440f3e68487
Mika Boersma <mika8117@gmail.com>
2015-03-19 11:52:24 +0100

    Initial

---
+++ Assets/PlayerController.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var moveLeft : KeyCode;
+var moveRight : KeyCode;
+var speed : float = 10;
+
+function Update () {
+	if (Input.GetKey(moveLeft)) {
+		rigidbody2D.velocity.x = speed *-1;
+	}
+	else if (Input.GetKey(moveRight)) {
+		rigidbody2D.velocity.x = speed;
+	}
+	else {
+	rigidbody2D.velocity.x = 0;
+	}
+}
