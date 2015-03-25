ea345031b72b64bec87604e243fe9bbd4a2dc3da
Mika Boersma <mika8117@gmail.com>
2015-03-25 20:38:46 +0100

    no message

---
+++ Assets/Reset.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var btnTexture : Texture;
+function OnGUI() {
+ GUI.backgroundColor = new Color(0,0,0,0);
+	if (!btnTexture) {
+		Debug.LogError("Please assign a texture on the inspector");
+		return;
+	}
+	if (GUI.Button(Rect(10,10,50,50),btnTexture)) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
